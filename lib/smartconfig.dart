23846313452e16799e32e4adf3c0f64ec2d377b9
Broto <brotoo25@gmail.com>
2021-09-04 23:51:31 -0300

    Migrate to null-safety

--- lib/smartconfig.dart
+++ lib/smartconfig.dart
@@ -8,7 +8,7 @@ class Smartconfig {
 
   static Future start(String ssid, String bssid, String pass) async {
     try {
-      Map<String, dynamic> rm = new Map<String, dynamic>.from(await _channel.invokeMethod('start', {"ssid": ssid, "bssid": bssid, "pass": pass}));
+      Map<String, dynamic> rm = new Map<String, dynamic>.from(await (_channel.invokeMethod('start', {"ssid": ssid, "bssid": bssid, "pass": pass}) as FutureOr<Map<dynamic, dynamic>>));
       return rm;
     } catch(err){
       print("Error, $err");
